8e8f3fbc1295d8c2d811f970acf55df799382464
Bahadur <bk209203@gmail.com>
2021-04-04 18:34:04 -0700

    05-04-2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { GallaryComponent } from './gallary/gallary.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GallaryComponent,
+    MainContentComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
